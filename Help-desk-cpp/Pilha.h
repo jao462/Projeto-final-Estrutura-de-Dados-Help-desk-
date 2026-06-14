a8ef82d17c81f1366f0423982659f9eb923e3924
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 15:10:23 -0300

    Add Pilha class and NoPilha struct definition

--- Help-desk-cpp/Pilha.h
+++ Help-desk-cpp/Pilha.h
@@ -1,1 +1,23 @@
+#ifndef PILHA_H
+#define PILHA_H
 
+#include <string>
+using namespace std;
+
+struct NoPilha {
+    string acao;
+    NoPilha* prox;
+};
+
+class Pilha {
+private:
+    NoPilha* topo;
+
+public:
+    Pilha();
+    void push(string acao);
+    void pop();
+    void mostrar();
+};
+
+#endif
