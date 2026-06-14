edd9e42dd8235549cfaa80b20306be9d3f6db49c
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 15:09:51 -0300

    Implement linked list methods in ListaEncadeada

--- Help-desk-cpp/ListaEncadead.cpp
+++ Help-desk-cpp/ListaEncadead.cpp
@@ -1,1 +1,39 @@
+#include "ListaEncadeada.h"
+#include <iostream>
+using namespace std;
 
+ListaEncadeada::ListaEncadeada() {
+    inicio = nullptr;
+}
+
+void ListaEncadeada::inserir(Ticket t) {
+    No* novo = new No{t, inicio};
+    inicio = novo;
+}
+
+void ListaEncadeada::listar() {
+    No* aux = inicio;
+
+    while (aux != nullptr) {
+        cout << "\nID: " << aux->dado.id;
+        cout << "\nNome: " << aux->dado.nome;
+        cout << "\nDescricao: " << aux->dado.descricao;
+        cout << "\nPrioridade: " << aux->dado.prioridade << "\n";
+        aux = aux->prox;
+    }
+}
+
+Ticket* ListaEncadeada::buscarPorId(int id) {
+    No* aux = inicio;
+
+    while (aux != nullptr) {
+        if (aux->dado.id == id)
+            return &aux->dado;
+        aux = aux->prox;
+    }
+    return nullptr;
+}
+
+No* ListaEncadeada::getInicio() {
+    return inicio;
+}
