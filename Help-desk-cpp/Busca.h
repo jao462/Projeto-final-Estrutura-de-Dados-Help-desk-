cff112ab770d44e0b701d5a948fa7424b3610ce5
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 17:29:53 -0300

    Delete Busca.h header file
    Remove Busca class header file.

--- Help-desk-cpp/Busca.h
+++ Help-desk-cpp/Busca.h
@@ -1,12 +1,1 @@
-#ifndef BUSCA_H
-#define BUSCA_H
 
-#include "Entidade.h"
-
-class Busca {
-public:
-    Ticket* sequencial(Ticket** v, int n, int id);
-    Ticket* binaria(Ticket** v, int n, int id);
-};
-
-#endif
