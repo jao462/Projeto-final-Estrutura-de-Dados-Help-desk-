d37801637fc01994633a179aa24aae8f7b39590f
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 15:09:04 -0300

    Add Ticket struct definition in Entidade.h

--- Help-desk-cpp/Entidade.h
+++ Help-desk-cpp/Entidade.h
@@ -1,1 +1,14 @@
+#ifndef ENTIDADE_H
+#define ENTIDADE_H
 
+#include <string>
+using namespace std;
+
+struct Ticket {
+    int id;
+    string nome;
+    string descricao;
+    int prioridade; // 1 baixa, 2 media, 3 alta
+};
+
+#endif
