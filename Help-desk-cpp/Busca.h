6d091d9084da29883b75fa6f41bc87375b1bb512
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 15:11:50 -0300

    Add Busca class with search methods for Ticket

--- Help-desk-cpp/Busca.h
+++ Help-desk-cpp/Busca.h
@@ -1,1 +1,12 @@
+#ifndef BUSCA_H
+#define BUSCA_H
 
+#include "Entidade.h"
+
+class Busca {
+public:
+    Ticket* sequencial(Ticket** v, int n, int id);
+    Ticket* binaria(Ticket** v, int n, int id);
+};
+
+#endif
