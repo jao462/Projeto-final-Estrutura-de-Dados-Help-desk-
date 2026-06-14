2d68c539e852dd1486846d61c17ffa285f0ba4ad
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 15:11:10 -0300

    Update Ordenacao.h

--- Help-desk-cpp/Ordenacao.h
+++ Help-desk-cpp/Ordenacao.h
@@ -1,1 +1,12 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
 
+#include "Entidade.h"
+
+class Ordenacao {
+public:
+    void bubbleSort(Ticket** v, int n);
+    void selectionSort(Ticket** v, int n);
+};
+
+#endif
