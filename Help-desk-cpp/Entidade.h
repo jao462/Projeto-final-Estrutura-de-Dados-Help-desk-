25ead9d46feac9a22a4dd1aa8e2a981f8eb8afa2
jao462 <piresjoaolucas772@gmail.com>
2026-06-14 17:30:01 -0300

    Update Entidade.h

--- Help-desk-cpp/Entidade.h
+++ Help-desk-cpp/Entidade.h
@@ -1,14 +1,1 @@
-#ifndef ENTIDADE_H
-#define ENTIDADE_H
 
-#include <string>
-using namespace std;
-
-struct Ticket {
-    int id;
-    string nome;
-    string descricao;
-    int prioridade; // 1 baixa, 2 media, 3 alta
-};
-
-#endif
